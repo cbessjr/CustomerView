32ec5dcc50dfa06295fac326922b35daf2f96591
Ender <cbessjr@gmail.com>
2018-12-23 01:39:11 -0700

    12232018 0140 schema.sql and seed.sql created

---
+++ schema.sql
@@ -0,0 +1,22 @@
+-- Check for database with existing name. Be careful not to run this if the
+-- databse has already been created.
+-- DROP DATABASE IF EXISTS bamazon;
+
+
+-- Database creation
+CREATE database bamazon;
+
+-- Database reference/use for the table creation
+USE bamazon;
+
+-- Database table creation
+CREATE TABLE products (
+  item_id INT (10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_name VARCHAR(100) NULL,
+  department_name VARCHAR (100),
+  price DECIMAL (10, 2),
+  stock_quantity INT (10)
+);
+
+-- Database display for verification all table columns were created successfully
+SELECT * FROM products;
