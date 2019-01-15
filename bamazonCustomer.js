412dad689dbe488a0266eb8b56386c0b2ef3322e
Ender <cbessjr@gmail.com>
2019-01-14 22:41:52 -0700

    011419 1045 user name and password update in the bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,8 +8,8 @@ var connection = mysql.createConnection({
 
     host: "localhost",
     port: 3306,
-    user: "root",
-    password: "Nothing5#####",
+    user: "",
+    password: "",
     database: "bamazon"
 
 });
